d66d923ce20551be2f58fb7f7b451aee054be660
navigaid <navigaid@gmail.com>
2022-04-05 16:45:11 +0800

    impl<T> ops::Try for Probably<T>

--- examples/main.rs
+++ examples/main.rs
@@ -1,22 +1,42 @@
-//! $ cargo run --example main; echo $?
-//! args = Nothing
+//! $ cargo run --example main
+//! some_args = Nothing
+//!
+//! $ echo $?
 //! 1
 //!
-//! $ cargo run --example main Probably Nothing; echo $?
-//! args = Something(["Probably", "Nothing"])
+//! $ cargo run --example main Probably Nothing
+//! some_args = Something(["Probably", "Nothing"])
+//! args = ["Probably", "Nothing"]
+//!
+//! $ echo $?
 //! 0
 //!
 use nothing::{Nothing, Probably, Something};
 
 fn get_args() -> Probably<Vec<String>> {
-    match std::env::args().collect::<Vec<String>>() {
-        args @ _ if args.len() > 1 => Something(args),
+    match std::env::args().skip(1).collect::<Vec<String>>() {
+        args @ _ if args.len() > 0 => Something(args),
         _ => Nothing,
     }
 }
 
 fn main() -> Probably<Vec<String>> {
-    let args = get_args();
+    let some_args = get_args();
+
+    println!("some_args = {some_args:?}");
+
+    // you can use ? operator to extract the inner value, and
+    // return immediately if it's Nothing. Roughly equivalent
+    // to the following code:
+    //
+    //   let args = match some_args {
+    //       Something(v) => v,
+    //       Nothing => return Nothing,
+    //   };
+
+    let args = some_args?;
+
     println!("args = {args:?}");
-    args
+
+    Something(args)
 }
--- lib.rs
+++ lib.rs
@@ -1,5 +1,3 @@
-#![feature(derive_default_enum)]
-
 //! nothing
 //! =======
 //!
@@ -7,7 +5,7 @@
 //! [![Documentation](https://docs.rs/nothing/badge.svg)](https://docs.rs/nothing)
 //! [![Build Status](https://travis-ci.org/btwiuse/nothing.svg?branch=master)](https://travis-ci.org/btwiuse/nothing)
 //!
-//! This is my own version of [Option](https://doc.rust-lang.org/stable/std/option/enum.Option.html). Definition:
+//! nothing::[Probably] is a better [Option].
 //!
 //! ```
 //! pub enum Probably<T> {
@@ -30,19 +28,25 @@
 //!
 //! Exit code is `0` if it is [Something], `1` if [Nothing].
 //!
-//! See [./examples/main.rs](https://github.com/btwiuse/nothing/blob/master/examples/main.rs)
+//! You can even use the `?` operator the way you would with [Option] and [Result]. See [./examples/main.rs](https://github.com/btwiuse/nothing/blob/master/examples/main.rs)
 //!
-//! ![Probably::Nothing](https://i.imgur.com/AuDdbOK.png)
+//! ![Probably::Nothing](https://camo.githubusercontent.com/8bfa566db90d366cb0dd026267f78a7dfca0c3193cb84172b90d05b594b7062c/68747470733a2f2f692e696d6775722e636f6d2f41754464624f4b2e706e67)
 //!
-//! It's probably nothing.
+//! Probably nothing.
+//!
 
-/// [Probably] is modelled after [Option]:
+#![feature(derive_default_enum)]
+#![feature(try_trait_v2)]
+
+/// [Probably] is a better [Option]:
 /// - [Something] is like [Some]
 /// - [Nothing] is like [None]
 #[derive(Clone, Copy, PartialOrd, Ord, PartialEq, Eq, Debug, Hash, Default)]
 pub enum Probably<T> {
+    /// No value.
     #[default]
     Nothing,
+    /// Some value of type `T`.
     Something(T),
 }
 
@@ -53,6 +57,31 @@ impl<T> std::process::Termination for Probably<T> {
         match self {
             Nothing => std::process::ExitCode::FAILURE,
             _ => std::process::ExitCode::SUCCESS,
+        }
+    }
+}
+
+impl<T> std::ops::FromResidual for Probably<T> {
+    fn from_residual(residual: Probably<std::convert::Infallible>) -> Self {
+        match residual {
+            Nothing => Nothing,
+            Something(_) => todo!(),
+        }
+    }
+}
+
+impl<T> std::ops::Try for Probably<T> {
+    type Output = T;
+    type Residual = Probably<std::convert::Infallible>;
+
+    fn from_output(output: Self::Output) -> Self {
+        Something(output)
+    }
+
+    fn branch(self) -> std::ops::ControlFlow<Self::Residual, Self::Output> {
+        match self {
+            Something(v) => std::ops::ControlFlow::Continue(v),
+            Nothing => std::ops::ControlFlow::Break(Nothing),
         }
     }
 }
