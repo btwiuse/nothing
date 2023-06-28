ebee00bce93d8a2899ed7d45fc8e080c1d7d89fc
navigaid <navigaid@gmail.com>
2023-06-28 17:35:29 +0000

    embed README.md as crate doc

--- lib.rs
+++ lib.rs
@@ -1,44 +1,5 @@
-//! nothing
-//! =======
-//!
-//! [![crates.io](https://img.shields.io/crates/v/nothing.svg)](https://crates.io/crates/nothing)
-//! [![Documentation](https://docs.rs/nothing/badge.svg)](https://docs.rs/nothing)
-//! [![Build Status](https://travis-ci.org/btwiuse/nothing.svg?branch=master)](https://travis-ci.org/btwiuse/nothing)
-//!
-//! nothing::[Probably] is a better [Option].
-//!
-//! ```
-//! pub enum Probably<T> {
-//!     Nothing,
-//!     Something(T),
-//! }
-//! ```
-//!
-//! ## Why?
-//!
-//! The point is that you can use [Probably] as the return type of your main function:
-//!
-//! ```
-//! use nothing::{Probably, Nothing};
-//!
-//! fn main() -> Probably<()> {
-//!     Nothing
-//! }
-//! ```
-//!
-//! Exit code is `0` if it is [Something], `1` if [Nothing].
-//!
-//! You can even use the `?` operator the way you would with [Option] and [Result]. See [./examples/main.rs](https://github.com/btwiuse/nothing/blob/master/examples/main.rs)
-//!
-//! ![Probably::Nothing](https://camo.githubusercontent.com/8bfa566db90d366cb0dd026267f78a7dfca0c3193cb84172b90d05b594b7062c/68747470733a2f2f692e696d6775722e636f6d2f41754464624f4b2e706e67)
-//!
-//! ## Not convinced?
-//!
-//! Real-world examples:
-//!
-//! - [that](https://crates.io/crates/that)
-//!
-//! Probably nothing.
+#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/README.md"))]
+
 #![feature(derive_default_enum)]
 #![feature(try_trait_v2)]
 
